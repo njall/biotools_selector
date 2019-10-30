b9d3bc25423ae6ab3a1d72410b0564d52b22ce62
Niall Beard <niallbeard@gmail.com>
2019-10-30 16:19:44 +0000

    Added production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+var path = require('path')
+var webpack = require('webpack')
+
+
 module.exports = {
   entry: './src/index.js',
   output: { 
     path: __dirname + '/dist',
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'build.js'
   },
   module: {
     rules: [
@@ -30,3 +34,35 @@ module.exports = {
     contentBase: './dist'
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+
+  var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+  module.exports.devtool = '#source-map'
+  
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+          'process.env': {
+              NODE_ENV: '"production"'
+          }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+          sourceMap: true,
+          compress: {
+              warnings: false
+          }
+      }),
+      new webpack.LoaderOptionsPlugin({
+          minimize: true
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: '../index.html',
+        version: gitRevisionPlugin.version(),
+        inject: false,
+      })
+  ])
+}
+
